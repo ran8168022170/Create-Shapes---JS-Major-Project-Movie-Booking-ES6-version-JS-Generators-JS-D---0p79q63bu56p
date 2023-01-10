98680884b8876c751d689032250c3d07004d455b
Ashish Kumar <entropy.ash@gmail.com>
2023-01-10 17:37:10 +0000

    Complete assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,35 @@
-import React from 'react'
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
+
 const App = () => {
+  const [shape, setShape] = useState("square");
+  const [shapesToBeAdded, setShapesToBeAdded] = useState([]);
+
+  const handleShapeChange = (event) => {
+    setShape(event.target.value);
+  };
+
+  const handleAddShapeButtonClick = () => {
+    setShapesToBeAdded([...shapesToBeAdded, shape]);
+  };
 
   return (
     <div id="main">
       <div id="shape-creator">
-        
-
+        <select onChange={handleShapeChange}>
+          <option value="square" selected={shape === 'square'}>Square</option>
+          <option value="circle" selected={shape === 'circle'}>Circle</option>
+        </select>
+      
+      <button onClick={handleAddShapeButtonClick}>Add shape</button>
       </div>
       <div id="shapes-holder">
+        {shapesToBeAdded.map((shape, index) => (
+          <div key={index} className={shape}></div>
+        ))}
       </div>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
